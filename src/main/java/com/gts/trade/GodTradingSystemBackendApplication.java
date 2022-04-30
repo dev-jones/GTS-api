caa504a3a8d015c972276a1f9e28b606753d8463
dev-jones <twry29@naver.com>
2022-04-30 10:29:03 +0900

    GTS-api 첫 커밋

---
+++ src/main/java/com/gts/trade/GodTradingSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gts.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodTradingSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodTradingSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gts/trade/GodTradingSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gts.trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodTradingSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
